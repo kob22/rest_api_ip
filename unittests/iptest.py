e1f4cea5bcc32bba9ec499f51cf2b078a4f4564b
kob22 <kob2222@gmail.com>
2018-08-30 13:24:00 +0200

    update tests for ip validator

--- unittests/iptest.py
+++ unittests/iptest.py
@@ -1,15 +1,23 @@
 import unittest
 from ip_validator import validate_address_ip
 
-class IpValidatorTestCase(unittest.unittest.TestCase):
+class IpValidatorTestCase(unittest.TestCase):
     """Tests for ip_validator.py"""
 
     def test_correct_ip(self):
         """Correct ip"""
-        ip_list  = ['211.125.65.7', '78.58.21.152', '179.201.92.102', '169.159.176.196', '196.236.110.1']
+        ip_list  = ['211.125.65.7', '78.58.21.152', '179.201.92.102', '169.159.176.196', '196.236.110.1', '008.2.5.6', '8.8.008.8']
         for ip in ip_list:
             validate_ip = validate_address_ip(ip)
-            self.assertTrue(validate_ip)
+            self.assertEqual(validate_ip, ip)
+
+    def test_correct_ip_with_digits(self):
+        """Correct ip with digits"""
+        ip_list  = ['008.2.5.6', '8.8.008.8', '001.002.004.000']
+        correct_ip_list = ['8.2.5.6', '8.8.8.8', '1.2.4.0']
+        for ip, correct_ip in zip(ip_list, correct_ip_list):
+            validate_ip = validate_address_ip(ip)
+            self.assertEqual(validate_ip, correct_ip_list)
 
     def test_only_digits_ip(self):
         """Incorrect ip - only digits"""
@@ -27,7 +35,9 @@ class IpValidatorTestCase(unittest.unittest.TestCase):
 
     def test_ip4_format(self):
         """test ipv4 format(four digits and four dots)"""
-        ip_list = ['38.73.21', '33.185.224,', '170,241.158.68', '135.199;145.109', '171.217.221-156']
+        ip_list = ['38.73.21', '33.185.224,', '170,241.158.68', '135.199;145.109', '171.217.221-156', '....', '...3.3', '....23']
         for ip in ip_list:
             validate_ip = validate_address_ip(ip)
             self.assertFalse(validate_ip)
+
+unittest.main()
